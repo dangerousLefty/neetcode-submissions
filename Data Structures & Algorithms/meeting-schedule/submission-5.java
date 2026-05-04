78dfe366c9da7667ac692f9b61d2f2e53e39a4c2
Hamza Khan <71505327+dangerousLefty@users.noreply.github.com>
2026-05-04 13:57:33 -0400

    Add: meeting-schedule - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule/submission-5.java
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        if (intervals.size() == 0) {return true;}
+
+        intervals.sort((a,b) -> Integer.compare(a.start, b.start));
+        Interval current = intervals.get(0);
+
+        for (int i = 1; i < intervals.size(); i++){
+            Interval next = intervals.get(i);
+
+            if (next.start < current.end){return false;}
+            current = next;
+        }
+
+        return true;
+    }
+}
